b32e590e24f8eb19c226553b96ee6b9b3f49678a
Unknown <cournalpwn@gmail.com>
2018-04-10 01:10:48 -0400

    Tumbling implemented and Groundcheck improved.
    Also lots of number tweaking.

--- Assets/Scripts/SimplePlatformController.cs
+++ Assets/Scripts/SimplePlatformController.cs
@@ -10,26 +10,19 @@ public class SimplePlatformController : MonoBehaviour
     public float moveForce = 30f;
     public float maxSpeed = 2f;
     public float jumpForce = 250f;
-    private bool grounded = false;
+    public float terminalVelocity = 5f;
     private float velocity = 0f;
-    private Transform groundCheck;
-
- 
+    private BoxCollider2D groundCheck;
     public float jumpPower = 0f;
-
-
-
-
+    private bool grounded = false;
+    private bool control = true;
     private Animator anim;
     private Rigidbody2D rb2d;
-
 
     // Use this for initialization
     void Awake()
     {
-
-        groundCheck = transform.Find("groundCheck");
-
+        Debug.Log("Test");
         anim = GetComponent<Animator>();
         rb2d = GetComponent<Rigidbody2D>();
     }
@@ -37,16 +30,15 @@ public class SimplePlatformController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        grounded = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
 
-        if (Input.GetButtonDown("Jump") && grounded)
+        if (Input.GetButtonDown("Jump") && grounded && control)
         {
-            jumpPower = jumpPower + 20f;
-            if (jumpPower == 300f)
-            {
-                jump = true;
-            }
-
+            /* jumpPower = jumpPower + 20f;
+             if (jumpPower == 300f)
+             {
+                 jump = true;
+             }*/
+            jump = true;
         }
 
     }
@@ -54,35 +46,46 @@ public class SimplePlatformController : MonoBehaviour
     void FixedUpdate()
     {
         float h = Input.GetAxis("Horizontal");
-        anim.SetFloat("Speed", Mathf.Abs(h));
-
-        if (h * rb2d.velocity.x < maxSpeed)
+        if (h * rb2d.velocity.x < maxSpeed && control)
+        {
             velocity = rb2d.velocity.x;
             rb2d.AddForce(Vector2.right * h * moveForce);
+        }
+            //rb2d.AddForce(Vector2.right * h * scale);
 
         if (Mathf.Abs(rb2d.velocity.x) > maxSpeed)
             rb2d.velocity = new Vector2(Mathf.Sign(rb2d.velocity.x) * maxSpeed, rb2d.velocity.y);
 
-        if (h > 0 && !facingRight)
+        if (Mathf.Abs(rb2d.velocity.y) > terminalVelocity)
+            rb2d.velocity = new Vector2(rb2d.velocity.x, Mathf.Sign(rb2d.velocity.y) * terminalVelocity);
+
+        if (h > 0 && !facingRight && control)
             Flip();
-        else if (h < 0 && facingRight)
+        else if (h < 0 && facingRight && control)
             Flip();
 
-
-
-        if (Mathf.Abs(rb2d.velocity.y) > 0.01)
+        //Determine what animation to play and whether the player currently has control
+        if (rb2d.velocity.y < -1 * (terminalVelocity-0.1) )
+        {
+            anim.SetTrigger("Tumble");
+            control = false;
+        }
+        else if (Mathf.Abs(rb2d.velocity.y) > 0.01)
         {
             anim.SetTrigger("Jump");
-
+            control = true;
         }
         else if (Mathf.Abs(rb2d.velocity.x) > 0.5)
         {
             anim.SetTrigger("Run");
+            control = true;
         }
         else
         {
             anim.SetTrigger("Idle");
+            control = true;
         }
+
         if (jump)
         {
 
@@ -93,6 +96,25 @@ public class SimplePlatformController : MonoBehaviour
         }
     }
 
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        Debug.Log("TriggerEnter");
+        if (other.gameObject.layer == LayerMask.NameToLayer("Ground"))
+        {
+            Debug.Log("Enter");
+            grounded = true;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D other)
+    {
+        Debug.Log("TriggerExit");
+        if (other.gameObject.layer == LayerMask.NameToLayer("Ground"))
+        {
+            Debug.Log("Exit");
+            grounded = false;
+        }
+    }
 
     void Flip()
     {
