8515b33b945c3ed909634d95f481079c1804d72c
jadsidi <jads3@vt.edu>
2018-04-15 20:44:14 -0400

    Name Entry Screen

---
+++ Assets/Scripts/InputText.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InputText : MonoBehaviour {
+
+	[SerializeField]
+	private InputField input;
+
+	void Awake()
+	{
+		//
+	}
+
+	public void GetInput(string entry)
+	{
+		Debug.Log (entry);
+		input.text = "";
+	}
+}
