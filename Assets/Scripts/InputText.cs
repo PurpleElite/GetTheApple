2fc68ee73602ddf6841f13b6fd26d645c757e64f
Unknown <cournalpwn@gmail.com>
2018-04-29 15:36:55 -0400

    Improved Name Entry
    it now looks fancier, the name entry box is autoselected, and you can hit the button by pressing enter. Also if you dont enter a name it defaults to "Anonymous"

--- Assets/Scripts/InputText.cs
+++ Assets/Scripts/InputText.cs
@@ -2,20 +2,48 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class InputText : MonoBehaviour {
 
+    private bool dumbVarToAddASlightDelay = false;
 	[SerializeField]
 	private InputField input;
 	void Awake()
 	{
-		//
+        input.Select();
+        input.ActivateInputField();
 	}
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Return))
+        {
+            FindObjectOfType<Button>().onClick.Invoke();
+        }
+        if (!input.isFocused)
+        {
+            input.Select();
+            dumbVarToAddASlightDelay = true;
+        }
+    }
+
+    void LateUpdate()
+    {
+        if (dumbVarToAddASlightDelay)
+        {
+            input.MoveTextEnd(true);
+            dumbVarToAddASlightDelay = false;
+        }
+    }
 
 	public void GetInput(string entry)
 	{
+        if (entry == "")
+        {
+            entry = "Anonymous";
+        }
         PlayerPrefs.SetString("Username", entry);
-        Debug.Log (entry);
 		input.text = entry;
 	}
 }
