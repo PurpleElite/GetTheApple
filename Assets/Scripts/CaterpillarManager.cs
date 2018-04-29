72db973a8c89286da613b5b37354966663b7f1fb
Unknown <cournalpwn@gmail.com>
2018-04-29 03:59:45 -0400

    More Caterpillar
    He now comments based on your position and also gets mad at you for cheating.

--- Assets/Scripts/CaterpillarManager.cs
+++ Assets/Scripts/CaterpillarManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -24,6 +25,7 @@ public class CaterpillarManager : MonoBehaviour {
     [SerializeField]
     private List<Transform> holes;
     private GameObject caterpillar;
+
     private GameObject activeClone;
     [SerializeField]
     private bool talking;
@@ -32,7 +34,15 @@ public class CaterpillarManager : MonoBehaviour {
 
     //Lots of variables to track various player actions
     private int numJumps;
-    private bool powerJump;
+
+    private bool powerJump = false;
+    private int currentZone;
+    private int lastZone;
+    private bool checkpoint1 = false;
+    private bool checkpoint2 = false;
+    private bool top = false;
+    private bool CHEATER = false;
+
 
 	// Use this for initialization
 	void Start () {
@@ -41,7 +51,10 @@ public class CaterpillarManager : MonoBehaviour {
         present = false;
         foreach (Transform child in transform)
         {
-            holes.Add(child);
+            if(child.gameObject.tag == "Hole")
+            {
+                holes.Add(child);
+            }
         }
         caterpillar = GameObject.FindGameObjectWithTag("Caterpillar");
         caterpillar.GetComponent<Animator>().enabled = false;
@@ -82,132 +95,221 @@ public class CaterpillarManager : MonoBehaviour {
     }
 
     public void FacePlant() {
-        Debug.Log(Time.time - StartGame.startTime);
         faceplantCount++;
-        if (faceplantCount == 1)
+        if (CHEATER)
+        {
+            lines.Enqueue(new DialogueLine("That's what you deserve. Cheater.", 5f));
+        }
+        else if (faceplantCount == 1)
         {
             lines.Enqueue(new DialogueLine("Hey there basket!\nAre you here to Get The Apple?", 5f));
-            lines.Enqueue(new DialogueLine("Well, don't you worry!\nIt's easy!", 5f));
-            lines.Enqueue(new DialogueLine("All you gotta do\nis climb up this tree!", 5f));
-            lines.Enqueue(new DialogueLine("It'd probably be\neasier if you had arms...", 5f));
-            lines.Enqueue(new DialogueLine("I'm rooting for you though!", 5f));
-            lines.Enqueue(new DialogueLine("Good Luck!", 3f));
-        }
-        else if (randomizer.Next(faceplantLineProb) == 0 && Time.time - StartGame.startTime > 360 && !talking)
-        {
-            if (randomizer.Next(faceplantLineProb) == 0)
-            {
-                switch (randomizer.Next(8))
-                {
-                    case 0:
-                        lines.Enqueue(new DialogueLine("*yawn*", 5f));
-                        break;
-                    case 1:
-                        lines.Enqueue(new DialogueLine("You still haven't gotten the apple?", 5f));
-                        break;
-                    case 2:
-                        lines.Enqueue(new DialogueLine("If you can't Get The Apple\nyou should just Give Up!", 7f));
-                        break;
-                    case 3:
-                        lines.Enqueue(new DialogueLine("You're not so good at this!", 5f));
-                        break;
-                    case 4:
-                        lines.Enqueue(new DialogueLine("Everyone watching you right now\nis very unimpressed.", 8f));
-                        break;
-                    case 5:
-                        lines.Enqueue(new DialogueLine("Here's a caterpillar tip:", 3f));
-                        lines.Enqueue(new DialogueLine("There are plenty of other exhibits\nhere at ICAT day.", 5f));
-                        lines.Enqueue(new DialogueLine("You should check those out too!", 5f));
-                        lines.Enqueue(new DialogueLine("Just remember to Give Up first.", 5f));
-                        break;
-                    case 6:
-                        lines.Enqueue(new DialogueLine("Give Up.", 8f));
-                        break;
-                    case 7:
-                        lines.Enqueue(new DialogueLine("Maybe perserverance can only\nget you so far in life?", 9f));
-                        break;
-                }
-            }
-        }
-        else if (randomizer.Next(faceplantLineProb) == 0 && Time.time - StartGame.startTime > 180 && !talking)
-        {
-            if (randomizer.Next(faceplantLineProb) == 0)
-            {
-                switch (randomizer.Next(6))
-                {
-                    case 0:
-                        lines.Enqueue(new DialogueLine("Maybe it's time to\ngive someone else a turn?", 5f));
-                        lines.Enqueue(new DialogueLine("All you have to do\nis Give Up.", 7f));
-                        break;
-                    case 1:
-                        lines.Enqueue(new DialogueLine("You are very persistent. Why?", 5f));
-                        break;
-                    case 2:
-                        lines.Enqueue(new DialogueLine("If at first you don't succeed,\njust Give Up.", 5f));
-                        break;
-                    case 3:
-                        lines.Enqueue(new DialogueLine("What are you trying to prove?", 5f));
-                        break;
-                    case 4:
-                        lines.Enqueue(new DialogueLine("Why do you want that apple so bad?", 6f));
-                        break;
-                    case 5:
-                        lines.Enqueue(new DialogueLine("Here's a caterpillar tip:", 3f));
-                        lines.Enqueue(new DialogueLine("The Give Up button\nis right there.", 5f));
-                        lines.Enqueue(new DialogueLine("You should press it!", 5f));
-                        break;
-                }
-            }
+            lines.Enqueue(new DialogueLine("Well, don't you worry!\nIt's easy!", 4f));
+            lines.Enqueue(new DialogueLine("All you gotta do\nis climb up this tree!", 4f));
+            lines.Enqueue(new DialogueLine("It'd probably be\neasier if you had arms...", 3f));
+            lines.Enqueue(new DialogueLine("I'm rooting for you though!", 4f));
+            lines.Enqueue(new DialogueLine("Good Luck!", 2f));
         }
         else if (!talking)
         {
-            if (randomizer.Next(faceplantLineProb) == 0)
+            if (randomizer.Next(faceplantLineProb) == 0 && Time.time - StartGame.startTime > 360)
             {
-                switch(randomizer.Next(11))
+                if (randomizer.Next(faceplantLineProb) == 0)
                 {
-                    case 0:
-                        lines.Enqueue(new DialogueLine("It's not about the destination\nIt's about the journey!", 7f));
-                        break;
-                    case 1:
-                        lines.Enqueue(new DialogueLine("Ouch!", 2f));
-                        lines.Enqueue(new DialogueLine("Remember that you can\ngive up with the GIVE UP button!\nJust press it with the mouse.", 7f));
-                        break;
-                    case 2:
-                        lines.Enqueue(new DialogueLine("Oh, close!", 4f));
-                        lines.Enqueue(new DialogueLine("You'll get to\nthe top in no time!", 5f));
-                        break;
-                    case 3:
-                        lines.Enqueue(new DialogueLine("That looked painful!", 5f));
-                        lines.Enqueue(new DialogueLine("Don't push yourself too hard, okay?", 5f));
-                        break;
-                    case 4:
-                        lines.Enqueue(new DialogueLine("Remember: if something is too hard,\nit's always okay to Give Up!", 7f));
-                        lines.Enqueue(new DialogueLine("I'm sure no one will judge you.", 5f));
-                        break;
-                    case 5:
-                        lines.Enqueue(new DialogueLine("Here's a caterpillar tip:", 3f));
-                        lines.Enqueue(new DialogueLine("Remember to use the\nspace bar or up arrow to jump!", 5f));
-                        break;
-                    case 6:
-                        lines.Enqueue(new DialogueLine("Don't push yourself too hard!", 5f));
-                        break;
-                    case 7:
-                        lines.Enqueue(new DialogueLine("Oof! That was quite the fall.", 5f));
-                        break;
-                    case 8:
-                        lines.Enqueue(new DialogueLine("Slow and steady wins the race.", 5f));
-                        break;
-                    case 9:
-                        lines.Enqueue(new DialogueLine("Here's a caterpillar tip:", 3f));
-                        lines.Enqueue(new DialogueLine("Always remember to eat a\nbalanced breakfast!", 5f));
-                        break;
-                    case 10:
-                        lines.Enqueue(new DialogueLine("Here's a caterpillar tip:", 3f));
-                        lines.Enqueue(new DialogueLine("An apple a day keeps\n the doctor away!", 6f));
-                        break;
+                    switch (randomizer.Next(8))
+                    {
+                        case 0:
+                            lines.Enqueue(new DialogueLine("*yawn*", 3f));
+                            break;
+                        case 1:
+                            lines.Enqueue(new DialogueLine("You still haven't gotten the apple?", 3f));
+                            break;
+                        case 2:
+                            lines.Enqueue(new DialogueLine("If you can't Get The Apple\nyou should just Give Up!", 7f));
+                            break;
+                        case 3:
+                            lines.Enqueue(new DialogueLine("You're not so good at this!", 5f));
+                            break;
+                        case 4:
+                            lines.Enqueue(new DialogueLine("Everyone watching you right now\nis very unimpressed.", 8f));
+                            break;
+                        case 5:
+                            lines.Enqueue(new DialogueLine("Here's a caterpillar tip:", 3f));
+                            lines.Enqueue(new DialogueLine("There are plenty of other exhibits\nhere at ICAT day.", 5f));
+                            lines.Enqueue(new DialogueLine("You should check those out too!", 5f));
+                            lines.Enqueue(new DialogueLine("Just remember to Give Up first.", 5f));
+                            break;
+                        case 6:
+                            lines.Enqueue(new DialogueLine("Give Up.", 8f));
+                            break;
+                        case 7:
+                            lines.Enqueue(new DialogueLine("Maybe perserverance can only\nget you so far in life?", 7f));
+                            break;
+                    }
+                }
+            }
+            else if (randomizer.Next(faceplantLineProb) == 0 && Time.time - StartGame.startTime > 180)
+            {
+                if (randomizer.Next(faceplantLineProb) == 0)
+                {
+                    switch (randomizer.Next(6))
+                    {
+                        case 0:
+                            lines.Enqueue(new DialogueLine("Maybe it's time to\ngive someone else a turn?", 5f));
+                            lines.Enqueue(new DialogueLine("All you have to do\nis Give Up.", 7f));
+                            break;
+                        case 1:
+                            lines.Enqueue(new DialogueLine("You are very persistent. Why?", 4f));
+                            break;
+                        case 2:
+                            lines.Enqueue(new DialogueLine("If at first you don't succeed,\njust Give Up.", 5f));
+                            break;
+                        case 3:
+                            lines.Enqueue(new DialogueLine("What are you trying to prove?", 4f));
+                            break;
+                        case 4:
+                            lines.Enqueue(new DialogueLine("Why do you want that apple so bad?", 4f));
+                            break;
+                        case 5:
+                            lines.Enqueue(new DialogueLine("Here's a caterpillar tip:", 3f));
+                            lines.Enqueue(new DialogueLine("The Give Up button\nis right there.", 5f));
+                            lines.Enqueue(new DialogueLine("You should press it!", 3f));
+                            break;
+                    }
+                }
+            }
+            else
+            {
+                if (randomizer.Next(faceplantLineProb) == 0)
+                {
+                    switch (randomizer.Next(11))
+                    {
+                        case 0:
+                            lines.Enqueue(new DialogueLine("It's not about the destination\nIt's about the journey!", 4f));
+                            break;
+                        case 1:
+                            lines.Enqueue(new DialogueLine("Ouch!", 2f));
+                            lines.Enqueue(new DialogueLine("Remember that you can\ngive up with the GIVE UP button!\nJust press it with the mouse.", 7f));
+                            break;
+                        case 2:
+                            lines.Enqueue(new DialogueLine("Oh, close!", 2f));
+                            lines.Enqueue(new DialogueLine("You'll get to\nthe top in no time!", 3f));
+                            break;
+                        case 3:
+                            lines.Enqueue(new DialogueLine("That looked painful!", 2f));
+                            lines.Enqueue(new DialogueLine("Don't push yourself too hard, okay?", 3f));
+                            break;
+                        case 4:
+                            lines.Enqueue(new DialogueLine("Remember: if something is too hard,\nit's always okay to Give Up!", 7f));
+                            lines.Enqueue(new DialogueLine("I'm sure no one will judge you.", 3f));
+                            break;
+                        case 5:
+                            lines.Enqueue(new DialogueLine("Here's a caterpillar tip:", 3f));
+                            lines.Enqueue(new DialogueLine("Remember to use the\nspace bar or up arrow to jump!", 4f));
+                            break;
+                        case 6:
+                            lines.Enqueue(new DialogueLine("Don't push yourself too hard!", 3f));
+                            break;
+                        case 7:
+                            lines.Enqueue(new DialogueLine("Oof! That was quite the fall.", 3f));
+                            break;
+                        case 8:
+                            lines.Enqueue(new DialogueLine("Slow and steady wins the race.", 4f));
+                            break;
+                        case 9:
+                            lines.Enqueue(new DialogueLine("Here's a caterpillar tip:", 3f));
+                            lines.Enqueue(new DialogueLine("Always remember to eat a\nbalanced breakfast!", 4f));
+                            break;
+                        case 10:
+                            lines.Enqueue(new DialogueLine("Here's a caterpillar tip:", 3f));
+                            lines.Enqueue(new DialogueLine("An apple a day keeps\n the doctor away!", 4f));
+                            break;
+                    }
                 }
             }
         }
+    }
+    internal void ZoneEnter(int zone)
+    {
+        currentZone = zone;
+        if (zone == 4 && !CHEATER)
+        {
+            if (present)
+            {
+                Despawn();
+            }
+            lines = new Queue<DialogueLine>();
+            lines.Enqueue(new DialogueLine("Wha-? Hey! You're not supposed\nto be able to get there!", 4f));
+            lines.Enqueue(new DialogueLine("Fine, I'll just have to remove the apple.", 4f));
+            Destroy(GameObject.FindGameObjectWithTag("Apple"), 7.5f);
+            lines.Enqueue(new DialogueLine("No apple for you.", 4f));
+            lines.Enqueue(new DialogueLine("Cheater.", 4f));
+            lines.Enqueue(new DialogueLine("Honestly, the nerve.", 4f));
+            lines.Enqueue(new DialogueLine("You think you can just go around beating games that are supposed to be impossible?", 4f));
+            lines.Enqueue(new DialogueLine("Think again. Doing stuff like that has CONSEQUENCES. So just Give Up because" +
+                "now the game is even more impossible than it was before.\n" +
+                "I am just so upset that you would do this. Just up and try to steal an apple. Did it ever occur to you that maybe that apple belonged to someone??\n" +
+                "No, of course not, you didn't think that. You just waltzed in here expecting to take apple and leave victorious. No such luck buddy chum pal.\n" +
+                "Now go press the Give Up button and give someone who isn't a CHEATER a try.", 8f));
+            CHEATER = true;
+        }
+        if (faceplantCount > 1 && !CHEATER)
+        {
+            if (zone == 1)
+            {
+                if (top)
+                {
+                    top = false;
+                    switch(randomizer.Next(3))
+                    {
+                        case 0:
+                            lines.Enqueue(new DialogueLine("Oof, back to the start.", 3f));
+                            lines.Enqueue(new DialogueLine("If you don't feel like climbing back up\n remember that Giving Up is an option!", 7f));
+                            break;
+                        case 1:
+                            lines.Enqueue(new DialogueLine("It's always rough to lose progress.", 4f));
+                            lines.Enqueue(new DialogueLine("After a fall like that/na lot of people Give Up.", 5f));
+                            lines.Enqueue(new DialogueLine("There's no shame in that.", 3f));
+                            break;
+                        case 2:
+                            lines.Enqueue(new DialogueLine("Ah! So close!", 3f));
+                            lines.Enqueue(new DialogueLine("You gave it your best,\nmaybe it's time to Give Up?", 6f));
+                            break;
+                    }
+                }
+            }
+            if (zone == 2)
+            {
+                if (!checkpoint1)
+                {
+                    checkpoint1 = true;
+                    lines.Enqueue(new DialogueLine("You're halfway there!", 3f));
+                    lines.Enqueue(new DialogueLine("Keep going!", 2f));
+                }
+            }
+            if (zone == 3)
+            {
+                top = true;
+                if (!checkpoint2)
+                {
+                    checkpoint2 = true;
+                    lines.Enqueue(new DialogueLine("There's the apple,\nit's so close now!", 3f));
+                    lines.Enqueue(new DialogueLine("I'm proud of you for\nmaking it this far.", 3f));
+                    lines.Enqueue(new DialogueLine("Now Get the Apple!", 2f));
+                }
+                else if (!top)
+                {
+                    lines.Enqueue(new DialogueLine("And now you're back up here again.", 3f));
+                    lines.Enqueue(new DialogueLine("Maybe you'll get it this time?", 3f));
+                }
+            }
+        }
+    }
+
+
+    internal void ZoneExit(int zone)
+    {
+        currentZone = 0;
+        lastZone = zone;
     }
 
     private void Despawn()
---
+++ Assets/Scripts/TriggerZone.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerZone : MonoBehaviour {
+
+    public int ID = 0;
+    private GameObject caterpillar;
+
+    // Use this for initialization
+    void Start () {
+        caterpillar = GameObject.FindGameObjectWithTag("CaterpillarManager");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Player") && !other.isTrigger)
+        {
+            caterpillar.GetComponent<CaterpillarManager>().ZoneEnter(ID);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Player") && !other.isTrigger)
+        {
+            caterpillar.GetComponent<CaterpillarManager>().ZoneExit(ID);
+        }
+    }
+}
